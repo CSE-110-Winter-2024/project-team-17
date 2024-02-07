27372877094feb17112eb6f6883cb9e37f718f0a
mlin1026 <97493164+mlin1026@users.noreply.github.com>
2024-02-06 18:24:38 -0800

    Update build.gradle.kts
    Update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
     project(":lib")
-    implementation(":lib")
+    implementation(project(":lib"))
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
